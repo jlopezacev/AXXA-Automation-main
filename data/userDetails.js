dc871f50b43fe7953cab482a028815038e34dd27
Jorge Lopez <jorgelopezacev@gmail.com>
2024-07-18 18:31:29 +0200

    Resolved userdetails and verbose

--- data/userDetails.js
+++ data/userDetails.js
@@ -5,8 +5,8 @@ export const adminDetails = {
     password:process.env.ADMIN_PASSWORD
     // username:"lab",
     // password:process.env.ADMIN_PASSWORD
-    username:"juan.lopez.external@axa.com",
-    password:process.env.ADMIN_PASSWORD
+    //username:"juan.lopez.external@axa.com",
+    //password:process.env.ADMIN_PASSWORD
 }
 //ADMIN_PASSWORD //VARIABLE ENVIRONMENT
 //process.env.ADMIN_PASSWORD
